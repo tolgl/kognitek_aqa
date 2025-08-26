74a953712693ff755453eac560e6adc733f331de
Anatoly Glinkin <tolyanbigam@gmail.com>
2025-08-26 16:52:30 +0300

    methods check status checkbox blue and click button open new window

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,3 +6,12 @@ class MainPageHelper(BasePage):
 
     def check_status_checkbox_orange(self):
         return self.find_element(MainPageLocators.checkbox_orange).is_selected()
+
+    def click_checkbox_blue(self):
+        self.find_element(MainPageLocators.checkbox_blue).click()
+
+    def check_status_checkbox_blue(self):
+        return self.find_element(MainPageLocators.checkbox_blue).is_selected()
+
+    def click_button_open_new_window(self):
+        self.find_element(MainPageLocators.button_open_new_window).click()
