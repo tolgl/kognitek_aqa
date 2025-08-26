250f75ff428dab4961dbca03a6156e221cd7d942
Anatoly Glinkin <tolyanbigam@gmail.com>
2025-08-26 16:52:53 +0300

    locators checkbox_blue and button_open_new_window

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -3,3 +3,5 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     checkbox_orange = (By.XPATH, ".//div[@class='widget-content']/input[@value='orange']")
+    checkbox_blue = (By.XPATH, ".//div[@class='widget-content']/input[@value='blue']")
+    button_open_new_window = (By.XPATH, ".//a[text()='Open a popup window']")
