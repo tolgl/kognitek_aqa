dc26ef3cb67750e76e55004b4af52a8b10a5ccfb
Anatoly Glinkin <tolyanbigam@gmail.com>
2025-08-26 16:55:54 +0300

    test_open_new_window

---
+++ tests/test_open_new_window.py
@@ -0,0 +1,18 @@
+from pages.main_page import MainPageHelper
+from pages.base_page import BasePage
+from pages.popup_window_page import PopupWindowPageHelper
+
+
+class TestCheckStatusCheckbox:
+
+    def test_open_new_window(self, driver):
+        base_page = BasePage(driver)
+        base_page.go_to_page()
+        main_page = MainPageHelper(driver)
+        main_page.click_button_open_new_window()
+        base_page.switch_to_window(count=2, index_win=1)
+        popup_window = PopupWindowPageHelper(driver)
+        text_h3_new_window = popup_window.get_text_h3()
+
+        assert text_h3_new_window == "New Window"
+
