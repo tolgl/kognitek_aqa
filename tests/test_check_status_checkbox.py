c8ad4833d0800f2955bc59e0e93815119db8857a
Anatoly Glinkin <tolyanbigam@gmail.com>
2025-08-26 16:53:54 +0300

    test click checkbox blue and check status

--- tests/test_check_status_checkbox.py
+++ tests/test_check_status_checkbox.py
@@ -11,3 +11,12 @@ class TestCheckStatusCheckbox:
         status_checkbox_orange = main_page.check_status_checkbox_orange()
 
         assert status_checkbox_orange is True
+
+    def test_click_checkbox_blue_and_check_status(self, driver):
+        base_page = BasePage(driver)
+        base_page.go_to_page()
+        main_page = MainPageHelper(driver)
+        main_page.click_checkbox_blue()
+        status_checkbox_blue = main_page.check_status_checkbox_blue()
+
+        assert status_checkbox_blue is True
