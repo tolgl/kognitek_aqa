c0eb0abf87cf6cc59e0d6b1165956c0aab71f930
Anatoly Glinkin <tolyanbigam@gmail.com>
2025-08-27 15:02:02 +0300

    delete method wait_open_new_window

--- pages/base_page.py
+++ pages/base_page.py
@@ -17,17 +17,13 @@ class BasePage:
     def find_element(self, locator, wait_time=5):
         return WebDriverWait(self.driver, wait_time).until(expected_conditions.presence_of_element_located(locator))
 
-    def wait_open_new_window(self, count, wait_time=5):
+    def switch_to_window(self, count, index_win, wait_time=5):
         WebDriverWait(self.driver, wait_time).until(
             expected_conditions.number_of_windows_to_be(count))
-
-    def switch_to_window(self, count, index_win):
-        self.wait_open_new_window(count)
         self.driver.switch_to.window(self.driver.window_handles[index_win])
 
     def close_window(self, count, index_win):
-        self.wait_open_new_window(count)
-        self.driver.switch_to.window(self.driver.window_handles[index_win])
+        self.switch_to_window(count, index_win)
         self.driver.close()
 
     def count_window(self):
