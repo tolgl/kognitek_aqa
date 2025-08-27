8bae51e465180c63d2bc3b9a424f27fee38c44c1
Anatoly Glinkin <tolyanbigam@gmail.com>
2025-08-27 14:49:21 +0300

    locators text_double_click and context_menu

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -5,3 +5,5 @@ class MainPageLocators:
     checkbox_orange = (By.XPATH, ".//div[@class='widget-content']/input[@value='orange']")
     checkbox_blue = (By.XPATH, ".//div[@class='widget-content']/input[@value='blue']")
     button_open_new_window = (By.XPATH, ".//a[text()='Open a popup window']")
+    text_double_click = (By.ID, "testdoubleclick")
+    context_menu = (By.ID, "myDropdown")
